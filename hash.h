a641646055d5e5a3af409a7702ddf2f0e78607bb
LaironSk <matsaueressig@hotmail.com>
2023-02-20 16:21:27 -0300

    aaa

--- hash.h
+++ hash.h
@@ -16,11 +16,51 @@ typedef struct Ht_item
     content** Ht_item_content;
 } Ht_item;
 
-// Defines the HashTable.
+typedef struct LinkedList
+{
+    Ht_item *item;
+    LinkedList *next;
+} LinkedList;
+
 typedef struct HashTable
 {
     // Contains an array of pointers to items.
-    Ht_item** items;
+    Ht_item **items;
+    LinkedList **overflow_buckets;
     int size;
     int count;
 } HashTable;
+
+unsigned long hash_function(char* str);
+
+LinkedList *allocate_list();
+
+LinkedList *linkedlist_insert(LinkedList *list, Ht_item *item);
+
+Ht_item *linkedlist_remove(LinkedList *list);
+
+void free_linkedlist(LinkedList *list);
+
+LinkedList **create_overflow_buckets(HashTable *table);
+
+void free_overflow_buckets(HashTable *table);
+
+Ht_item* create_item(char* key, char* value);
+
+HashTable* create_table(int size);
+
+void free_item(Ht_item* item);
+
+void free_table(HashTable* table);
+
+void handle_collision(HashTable *table, unsigned long index, Ht_item *item);
+
+void ht_insert(HashTable* table, char* key, char* value);
+
+char* ht_search(HashTable* table, char* key);
+
+void ht_delete(HashTable *table, char *key);
+
+void print_search(HashTable *table, char *key);
+
+void print_table(HashTable *table);
