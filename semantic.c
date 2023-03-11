3ac95fa409442ad1d52173f386e7b7dfe26e9794
LaironSk <matsaueressig@hotmail.com>
2023-03-11 19:36:42 -0300

    semantada

--- semantic.c
+++ semantic.c
@@ -1,3 +1,38 @@
-#include "semantic.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "asd.h"
+#include "semantic.h"
+
+
+
+int undeclared_err (value_t value1)
+{}
+
+int declared_err (value_t value1)
+{}
+
+int variable_err (value_t value1)
+{}
+
+int array_err (value_t value1)
+{}
+
+int function_err (value_t value1)
+{}
+
+int char_to_int_err (value_t value1, value_t value2)
+{}
+
+int char_to_float (value_t value1, value_t value2)
+{}
+
+int char_to_bool (value_t value1, value_t value2)
+{}
+
+int char_vector (value_t value1, value_t value2)
+{}
+
+int x_to_char (value_t value1, value_t value2)
+{}
---
+++ semantic.h
@@ -0,0 +1,30 @@
+#define ERR_UNDECLARED 10 //2.2
+#define ERR_DECLARED 11 //2.2
+#define ERR_VARIABLE 20 //2.3
+#define ERR_ARRAY 21 //2.3
+#define ERR_FUNCTION 22 //2.3
+#define ERR_CHAR_TO_INT 31 //2.4
+#define ERR_CHAR_TO_FLOAT 32 //2.4
+#define ERR_CHAR_TO_BOOL 33 //2.4
+#define ERR_CHAR_VECTOR 34 //2.4
+#define ERR_X_TO_CHAR 35 //2.4
+
+int undeclared_err (value_t value1);
+
+int declared_err (value_t value1);
+
+int variable_err (value_t value1);
+
+int array_err (value_t value1);
+
+int function_err (value_t value1);
+
+int char_to_int_err (value_t value1, value_t value2);
+
+int char_to_float (value_t value1, value_t value2);
+
+int char_to_bool (value_t value1, value_t value2);
+
+int char_vector (value_t value1, value_t value2);
+
+int x_to_char (value_t value1, value_t value2);
