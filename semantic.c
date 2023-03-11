faaa3b7e17d3380f3e2457fb0b31e35683502c35
LaironSk <matsaueressig@hotmail.com>
2023-03-11 19:59:56 -0300

    Update semantic.c

--- semantic.c
+++ semantic.c
@@ -8,13 +8,25 @@
 
 
 int undeclared_err (value_t value1)
-{}
+{
+	while(i != -1 or ht_search != NULL;)
+		ht_search(value1);
+	
+	return ERR_UNDECLARED;
+}
 
 int declared_err (value_t value1)
-{}
+{
+	while(i != -1 or ht_search != NULL;)
+		ht_search(value1);
+	
+	return ERR_DECLARED;
+}
 
 int variable_err (value_t value1)
-{}
+{
+	
+}
 
 int array_err (value_t value1)
 {}
@@ -23,16 +35,32 @@ int function_err (value_t value1)
 {}
 
 int char_to_int_err (value_t value1, value_t value2)
-{}
+{
+	if(Ht_item1.type != Ht_item2.type)
+		return ERR_CHAR_TO_INT;
+	
+}
 
 int char_to_float (value_t value1, value_t value2)
-{}
+{
+	if(Ht_item1.type != Ht_item2.type)
+		return ERR_CHAR_TO_FLOAT;
+}
 
 int char_to_bool (value_t value1, value_t value2)
-{}
+{
+	if(Ht_item1.type != Ht_item2.type)
+		return ERR_CHAR_TO_BOOL;
+}
 
 int char_vector (value_t value1, value_t value2)
-{}
+{
+	if(Ht_item1.type != Ht_item2.type)
+		return ERR_CHAR_VECTOR;
+}
 
 int x_to_char (value_t value1, value_t value2)
-{}
+{
+	if(Ht_item1.type != Ht_item2.type)
+		return ERR_X_TO_CHAR;
+}
