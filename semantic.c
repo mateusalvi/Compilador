367cfc1d06ba60f28c4f5c49c474b8247679c98a
LaironSk <matsaueressig@hotmail.com>
2023-03-12 02:05:50 -0300

    Update semantic.c

--- semantic.c
+++ semantic.c
@@ -1,66 +1,66 @@
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include "asd.h"
-// #include "semantic.h"
+ #include <stdio.h>
+#include <stdlib.h>
+ #include <string.h>
+ #include "asd.h"
+ #include "semantic.h"
 
 
 
-// int undeclared_err (value_t value1)
-// {
-// 	while(i != -1 or ht_search == NULL;)
-// 		ht_search(value1);
+int undeclared_err (value_t value1)
+ {
+ 	while(i != -1 or ht_search == NULL;)
+ 		ht_search(value1);
 	
-// 	return ERR_UNDECLARED;
-// }
+ 	return ERR_UNDECLARED;
+ }
 
-// int declared_err (value_t value1)
-// {
-// 	while(i != -1 or ht_search == NULL;)
-// 		ht_search(value1);
+ int declared_err (value_t value1)
+ {
+ 	while(i != -1 or ht_search == NULL;)
+ 		ht_search(value1);
 	
-// 	return ERR_DECLARED;
-// }
+ 	return ERR_DECLARED;
+ }
 
-// int variable_err (value_t value1)
-// {
-// 	return ERR_VARIABLE
-// }
+ int variable_err (value_t value1)
+ {
+ 	return ERR_VARIABLE
+ }
 
-// int array_err (value_t value1)
-// {}
+ int array_err (value_t value1)
+ {}
 
-// int function_err (value_t value1)
-// {}
+ int function_err (value_t value1)
+ {}
 
-// int char_to_int_err (value_t value1, value_t value2)
-// {
-// 	if(Ht_item1.type != Ht_item2.type)
-// 		return ERR_CHAR_TO_INT;
+ int char_to_int_err (value_t value1, value_t value2)
+ {
+ 	if(Ht_item1.type != Ht_item2.type)
+		return ERR_CHAR_TO_INT;
 	
-// }
+ }
 
-// int char_to_float (value_t value1, value_t value2)
-// {
-// 	if(Ht_item1.type != Ht_item2.type)
-// 		return ERR_CHAR_TO_FLOAT;
-// }
+int char_to_float (value_t value1, value_t value2)
+ {
+	if(Ht_item1.type != Ht_item2.type)
+ 		return ERR_CHAR_TO_FLOAT;
+ }
 
-// int char_to_bool (value_t value1, value_t value2)
-// {
-// 	if(Ht_item1.type != Ht_item2.type)
-// 		return ERR_CHAR_TO_BOOL;
-// }
+ int char_to_bool (value_t value1, value_t value2)
+ {
+ 	if(Ht_item1.type != Ht_item2.type)
+ 		return ERR_CHAR_TO_BOOL;
+ }
 
-// int char_vector (value_t value1, value_t value2)
-// {
-// 	if(Ht_item1.type != Ht_item2.type)
-// 		return ERR_CHAR_VECTOR;
-// }
+ int char_vector (value_t value1, value_t value2)
+ {
+ 	if(Ht_item1.type != Ht_item2.type)
+ 		return ERR_CHAR_VECTOR;
+ }
 
-// int x_to_char (value_t value1, value_t value2)
-// {
-// 	if(Ht_item1.type != Ht_item2.type)
-// 		return ERR_X_TO_CHAR;
-// }
+ int x_to_char (value_t value1, value_t value2)
+ {
+ 	if(Ht_item1.type != Ht_item2.type)
+ 		return ERR_X_TO_CHAR;
+ }
