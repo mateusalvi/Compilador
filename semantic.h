6ace3fa66157eeb7823754627a21bc40e830a825
LaironSk <matsaueressig@hotmail.com>
2023-03-15 00:05:22 -0300

    er

--- semantic.h
+++ semantic.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // #define ERR_UNDECLARED 10 //2.2
 // #define ERR_DECLARED 11 //2.2
 // #define ERR_VARIABLE 20 //2.3
@@ -18,6 +19,20 @@
 // void compare_type(value_t valor1, value_t valor2);
 // /*
 // int undeclared_err (value_t value1);
+=======
+#define ERR_UNDECLARED 10 //2.2
+#define ERR_DECLARED 11 //2.2
+#define ERR_VARIABLE 20 //2.3
+#define ERR_ARRAY 21 //2.3
+#define ERR_FUNCTION 22 //2.3
+#define ERR_CHAR_TO_INT 31 //2.4
+#define ERR_CHAR_TO_FLOAT 32 //2.4
+#define ERR_CHAR_TO_BOOL 33 //2.4
+#define ERR_CHAR_VECTOR 34 //2.4
+#define ERR_X_TO_CHAR 35 //2.4
+/*
+int undeclared_err (value_t value1);
+>>>>>>> parent of 0475934 (Merge branch 'main' of https://github.com/mateusalvi/Compilador)
 
 // int declared_err (value_t value1);
 
